b6eadea76c7f863d6730e5a10c8d5fa4f8f02d53
Bornew <bornew@foxmail.com>
2021-04-01 17:19:56 +0800

    :sparkles: Finished html preview

--- frontend/BlockPreview.tsx
+++ frontend/BlockPreview.tsx
@@ -1,73 +1,129 @@
-import {Field, Table} from "@airtable/blocks/models";
-import {useRecordById} from "@airtable/blocks/ui";
+import { Field, Table } from "@airtable/blocks/models";
+import { useRecordById, Text, Box, Button, Icon } from "@airtable/blocks/ui";
 import React from "react";
 import ReactDOMServer from "react-dom/server";
-import sanitizeHtml from 'sanitize-html';
-import MarkdownIt from 'markdown-it';
-import {useSettings} from "./settings/settings";
+// import sanitizeHtml from "sanitize-html";
+import MarkdownIt from "markdown-it";
+import { useSettings } from "./settings/settings";
 
 const md = new MarkdownIt({
-    html: true,
-    linkify: true,
+  html: true,
+  linkify: true,
 });
 
 const defaultTextRender = md.renderer.rules.text;
 
 md.renderer.rules.text = function (tokens, idx, options, env, self) {
-    const token = tokens[idx];
-    const matches = token.content.match(/\[(X|\s|\_|\-)\]\s(.*)/i);
+  const token = tokens[idx];
+  const matches = token.content.match(/\[(X|\s|\_|\-)\]\s(.*)/i);
 
-    if (matches) {
-        token.content = matches[2];
-        return ReactDOMServer.renderToString(<span>
-            <input type="checkbox" checked={matches[1].trim()} disabled/>
-            <label>{defaultTextRender(tokens, idx, options, env, self)}</label>
-        </span>);
-    }
-    // pass token to default renderer.
-    return defaultTextRender(tokens, idx, options, env, self);
+  if (matches) {
+    token.content = matches[2];
+    return ReactDOMServer.renderToString(
+      <span>
+        <input type="checkbox" checked={matches[1].trim()} disabled />
+        <label>{defaultTextRender(tokens, idx, options, env, self)}</label>
+      </span>
+    );
+  }
+  // pass token to default renderer.
+  return defaultTextRender(tokens, idx, options, env, self);
 };
 
-export default function RecordPreview(props: { table: Table, recordId: string, field: Field }) {
-    const {table, recordId, field} = props;
+export default function BlockPreview(props: {
+  table: Table;
+  recordId: string;
+  field: Field;
+}) {
+  const { table, recordId, field } = props;
+  console.log("blockPreview");
+  const record = useRecordById(table, recordId, { fields: [field] });
+  if (!record) {
+    console.log("no Record");
+    return <p>"Error: Record not found."</p>;
+  }
 
-    const record = useRecordById(table, recordId, {fields: [field]});
-    if (!record) {
-        return <p>"Error: Record not found."</p>
-    }
-
-    const html = (record.getCellValue(field) || '').toString();
-    return <SanitizeHTML html={html}/>
+  const html = (record.getCellValue(field) || "").toString();
+  console.log(html);
+  return (
+    <Box>
+      <ShowHTML html={html} />
+      <Box
+        width="100%"
+        height="28px"
+        backgroundColor="lightGray1"
+        display="flex"
+        flexDirection="row"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Icon
+          name="plus"
+          size={16}
+          fillColor="hsl(0,0%,46%)"
+          marginRight="4px"
+        />
+        <Text textColor="light">Add</Text>
+      </Box>
+    </Box>
+  );
 }
 
-function SanitizeHTML({html}: { html: string }) {
-    const  includeMarkdown = true;
+function ShowHTML({ html }: { html: string }) {
+  html = html
+    .replace(/^(\s*)\[(X|\s|\_|\-)\]\s(.*)/gim, "$1- [$2] $3")
+    .replace(/\n\[(.{1})]/g, "\\\n[$1]");
 
-    if (includeMarkdown) {
-        html = html
-            .replace(/^(\s*)\[(X|\s|\_|\-)\]\s(.*)/gim, "$1- [$2] $3")
-            .replace(/\n\[(.{1})]/g, "\\\n[$1]");
-    }
-    const html2 = includeMarkdown ? md.render(html) : html;
+  const html2 = md.render(html);
 
-    const domParser = new DOMParser();
-    const tmpEl = domParser.parseFromString(html2, 'text/html');
-
-    const allowedTags = sanitizeHtml.defaults.allowedTags.concat(['h1', 'h2', 'u', 'label', 'input']);
-    const allowedAttributes = sanitizeHtml.defaults.allowedAttributes;
-    allowedAttributes['input'] = ['checked', 'type', 'disabled'];
-    allowedAttributes['ul'] = ['class'];
-    allowedAttributes['li'] = ['class'];
-
-    const links = tmpEl.querySelectorAll('a');
-    links.forEach((aEl) => {
-        if (!aEl.attributes.getNamedItem('target')) {
-            aEl.setAttribute('target', '_blank');
-        }
-    });
-
-    const htmlFixed = tmpEl.body.innerHTML;
-
-    return null;
+  const domParser = new DOMParser();
+  const tmpEl = domParser.parseFromString(html2, "text/html");
+  const htmlFixed = tmpEl.body.innerHTML;
+  console.log("htmlFixed", htmlFixed);
+  return (
+    <Box>
+      <style>
+        {`
+            .contains-task-list {
+                list-style: none;
+                padding: 0;
+            }
+            .contains-task-list ul {
+                padding-left: 2em;
+            }
+            input[type="checkbox"] {
+                visibility: hidden;
+                width: 0;
+            }
+            input[type="checkbox"] + label:before {
+                border: 2px solid rgba(0,0,0,0.25);
+                border-radius: 3px;
+                box-sizing: border-box;
+                content: "\\00a0";
+                display: inline-block;
+                left: -25px;
+                margin: 0 .5em 0 0;
+                padding: 0;
+                vertical-align: bottom;
+                top: 3.5px;
+                width: 12px;
+                height: 12px;
+            }
+            input[type="checkbox"]:checked + label:before {
+                border: 0;
+                background-color: rgb(45, 127, 249);
+                background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='12' height='12' viewBox='0 0 16 16' style='shape-rendering: geometricprecision;'%3E%3Cpath fill-rule='evenodd' fill='white' d='M5.944 12.305a1.031 1.031 0 0 0 1.433-.009l5.272-5.181A1.483 1.483 0 0 0 12.661 5a1.468 1.468 0 0 0-2.109.025L7.008 8.701a.465.465 0 0 1-.685-.01l-.587-.641A1.42 1.42 0 0 0 3.661 8a1.473 1.473 0 0 0 .017 2.106l2.266 2.199z'%3E%3C/path%3E%3C/svg%3E");
+                box-sizing: border-box;
+                color: rgb(119, 119, 119);
+                content: "\\2713";
+                text-align: center;
+            }
+            input[type="checkbox"]:focus + label::before {
+                outline: rgb(59, 153, 252) auto 5px;
+            }
+            `}
+      </style>
+      <div dangerouslySetInnerHTML={{ __html: htmlFixed }}></div>
+    </Box>
+  );
 }
-
--- frontend/PagePreview.tsx
+++ frontend/PagePreview.tsx
@@ -1,41 +1,90 @@
-import {Table, Field} from "@airtable/blocks/models";
-import {useBase, useRecordById, useLoadable, useSettingsButton, useWatchable, Box, Text, Heading, Icon, Button} from "@airtable/blocks/ui";
-import React, {useEffect, useState} from 'react';
-import {cursor} from "@airtable/blocks";
-import {allowedUrlFieldTypes, useSettings} from "./settings/settings";
-import BlockPreview from './BlockPreview';
-
-
+import { Table, Field } from "@airtable/blocks/models";
+import {
+  useBase,
+  useRecordById,
+  useLoadable,
+  useSettingsButton,
+  useWatchable,
+  Box,
+  Text,
+  Heading,
+  Icon,
+  Button,
+} from "@airtable/blocks/ui";
+import React, { useEffect, useState } from "react";
+import { cursor } from "@airtable/blocks";
+import { allowedUrlFieldTypes, useSettings } from "./settings/settings";
+import BlockPreview from "./BlockPreview";
 
 export default function PagePreview() {
-    // const {
-    //     settings: {table},
-    // } = useSettings();
-    let errorMessage = null;
-    const base = useBase();
-    useWatchable(cursor, ['selectedRecordIds', 'selectedFieldIds']);
-    const activeTable = base.getTableByIdIfExists(cursor.activeTableId);
-    const blockArrays: BlockInterface[] = [];
-    const selectedFieldIds = cursor.selectedFieldIds;
-    const selectedRecordIds = cursor.selectedRecordIds;
-    const selectedField = activeTable.getField(selectedFieldIds[0]);
-    
-    return(
-         <Box paddingX={4} paddingY={3} display="flex" flexDirection="column">
-             <div>
-                 <Heading marginBottom={4}>{activeTable.name}</Heading>
-                {selectedRecordIds.map((selectedRecordId) => 
-                    <BlockPreview table={activeTable} recordId={selectedRecordId} field={selectedField}/>).join('')}
-            </div>
-            <Box marginTop="16px" width="100%" height="28px" backgroundColor="lightGray1" display='flex' flexDirection="row" alignItems="center" justifyContent="center">
-                <Icon name="plus" size={16} fillColor="hsl(0,0%,46%)" marginRight="4px"/>
-                <Text textColor="light">Add</Text>
-            </Box>
-        </Box>
-    )
+  // const {
+  //     settings: {table},
+  // } = useSettings();
+  let errorMessage = null;
+  const base = useBase();
+  useWatchable(cursor, ["selectedRecordIds", "selectedFieldIds"]);
+  const activeTable = base.getTableByIdIfExists(cursor.activeTableId);
+  const [blockArrays, setBlockArrays] = useState<BlockInterface[]>([]);
+  const selectedFieldIds = cursor.selectedFieldIds;
+  const selectedRecordIds = cursor.selectedRecordIds;
+
+  const addBlocks = (selectedBlocks: BlockInterface[]) => {
+    const newBlockArrays = blockArrays.concat(selectedBlocks);
+    setBlockArrays(newBlockArrays);
+  };
+
+  useEffect(() => {
+    console.log("blockArrays", blockArrays);
+  }, [blockArrays]);
+  return (
+    <Box paddingX={4} paddingY={3} display="flex" flexDirection="column">
+      <div>
+        <Heading marginBottom={4}>{activeTable.name}</Heading>
+        {blockArrays.map((block) => (
+          <BlockPreview
+            table={block.table}
+            recordId={block.recordId}
+            field={block.field}
+          />
+        ))}
+      </div>
+      <Box
+        onClick={() => {
+          if (selectedRecordIds && selectedFieldIds) {
+            const selectedBlocks: BlockInterface[] = selectedRecordIds
+              .map((selectedRecordId) => ({
+                table: activeTable,
+                recordId: selectedRecordId,
+                field: activeTable.getField(selectedFieldIds[0]),
+              }))
+              .concat();
+            addBlocks(selectedBlocks);
+          } else {
+            console.log("error, no selected cells");
+          }
+        }}
+        marginTop="16px"
+        width="100%"
+        height="28px"
+        backgroundColor="lightGray1"
+        display="flex"
+        flexDirection="row"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Icon
+          name="plus"
+          size={16}
+          fillColor="hsl(0,0%,46%)"
+          marginRight="4px"
+        />
+        <Text textColor="light">Add</Text>
+      </Box>
+    </Box>
+  );
 }
 export interface BlockInterface {
-    table: Table,
-    recordId: string,
-    field: Field
+  table: Table;
+  recordId: string;
+  field: Field;
 }
